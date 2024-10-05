636d2fa10547419861a1b527a95a6eeb78c2f580
Ameline <amelinechia@MacBook-Air-2.local>
2024-10-05 15:25:04 +0800

    Setup urban heat project for version control

---
+++ code/main.R
@@ -0,0 +1,31 @@
+# Import libraries
+install.packages("tidyverse", dependencies = TRUE)
+install.packages("ggplot2", dependencies = TRUE)
+install.packages("sf", dependencies = TRUE)
+install.packages("tmap", dependencies = TRUE)
+install.packages("measurements", dependencies = TRUE)
+install.packages("spData",dependencies = TRUE)
+
+library(tidyverse)
+library(ggplot2)
+library(sf)
+library(tmap)
+library(measurements)
+library(spData)
+# 1. DATA PROCESSING
+## Datasets merger
+
+## Data cleaning
+### Checking for NA / NAN / empty values
+### Removing data that doesnt value add the analysis (if required)
+### Removing data with special characters (if required)
+
+# 2. DATA TRANSFORMATION
+### Checking and modifying the data types of each columns
+### Mathematical transformation on data columns (if required)
+
+
+# 2. EXPLORATORY DATA ANALYSIS (EDA)
+
+
+# 3. MODELLING ANALYSIS
