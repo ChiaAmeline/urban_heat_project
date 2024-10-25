8b951eff1e37fc3b7aaf3927f9c5e24391d30cfa
Rakhshana Parveen <parveenrakhshana@hmail.com>
2024-10-26 00:17:52 +0800

    code for linking csv / excel files to R from google drive

--- code/main.R
+++ code/main.R
@@ -5,6 +5,8 @@ install.packages("sf", dependencies = TRUE)
 install.packages("tmap", dependencies = TRUE)
 install.packages("measurements", dependencies = TRUE)
 install.packages("spData",dependencies = TRUE)
+install.packages("googledrive", dependencies = TRUE)
+install.packages("readxl")
 
 library(tidyverse)
 library(dplyr)
@@ -13,8 +15,68 @@ library(sf)
 library(tmap)
 library(measurements)
 library(spData)
+library(googledrive)
+library(readxl)
 
-# 1. DATA PROCESSING
+# 1. DATA CONNECTION VIA GOOGLE DRIVE
+
+#Using Google Drive to connect to the csv files as advised by Prof. Pls install the GoogleDrive package above before running this chunk.
+
+##### File ID from Google Drive URL (For GlobalLandTemperaturesByCity)
+file_id_temp_cities <- "17lPSwGwt5HTMbIiaTkotfmuKlkKZAPcM"
+
+# Download the file to a temporary location
+temp_cities <- tempfile(fileext = ".csv")
+drive_download(as_id(file_id_temp_cities), path = temp_cities, overwrite = TRUE)
+
+
+# Read the CSV file from the temporary location
+temp_cities_data <- read.csv(temp_cities)
+
+##### File ID from Google Drive URL (For GlobalLandTemperaturesByCountry)
+file_id_temp_country <- "1eUG98W8Mz6OURXim17SEYiFFtsz3oGpG"
+
+# Download the file to a temporary location
+temp_country <- tempfile(fileext = ".csv")
+drive_download(as_id(file_id_temp_country), path = temp_country, overwrite = TRUE)
+
+
+# Read the CSV file from the temporary location
+temp_country_data <- read.csv(temp_country)
+
+##### File ID from Google Drive URL (For world_population)
+file_id_temp_pop <- "1sAMaGYknHeDDwdTICCsqgfWBXcmw5Wtl"
+
+# Download the file to a temporary location
+temp_pop <- tempfile(fileext = ".csv")
+drive_download(as_id(file_id_temp_pop), path = temp_pop, overwrite = TRUE)
+
+# Read the CSV file from the temporary location
+population_data <- read.csv(temp_pop)
+
+
+##### File ID from Google Drive URL (For energy)
+file_id_temp_energy <- "1Oha-hiaJZCH9d4jJW-SN5GqcUJew9s1Z"
+
+# Download the file to a temporary location
+temp_energy <- tempfile(fileext = ".csv")
+drive_download(as_id(file_id_temp_energy), path = temp_energy, overwrite = TRUE)
+
+# Read the CSV file from the temporary location
+energy_data <- read.csv(temp_energy)
+
+
+##### File ID from Google Drive URL (For Share_of_green_areas_and_green_area_per_capita_in_cities_and_urban_areas_1990_-_2020)
+file_id_temp_green <- "1Yu75p6z9dXVbfIe9n2rb9QU7YBntvsyz"
+
+# Download the file to a temporary location with .xlsx extension
+temp_green <- tempfile(fileext = ".xlsx")
+drive_download(as_id(file_id_temp_green), path = temp_green, overwrite = TRUE)
+
+# Read the Excel file from the temporary location
+green_area_data <- read_excel(temp_green)
+
+
 ## Datasets merger
 #test new shana
 
